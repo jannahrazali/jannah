d91803f1e1f24e3f53040380731c24c8a75c9dc1
Jannah Razali <139848564+jannahrazali@users.noreply.github.com>
2023-08-15 17:03:22 +0800

    Add files via upload

---
+++ inheritance/Ayam.java
@@ -0,0 +1,7 @@
+// Ayam class inherits from the Makan class.
+
+public class Ayam extends Makan{
+    int berapa = 2;
+    int habis = 2;
+}
+
---
+++ inheritance/Makan.java
@@ -0,0 +1,15 @@
+//class and method Makan
+public class Makan {
+    double eat;
+
+    // Method indicate makan lunch
+    void lunch(){
+        System.out.println("makan lunch");
+    }
+
+    // Method indicate makan breakfast
+    void breakfast(){
+        System.out.println("makan breakfast");
+    }
+}
+
